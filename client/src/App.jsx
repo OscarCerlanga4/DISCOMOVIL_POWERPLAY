98318249090becfaa00ae9f79ecb02db2d40c755
OscarCerlanga4 <oscarcerlanga@iesrioarba.es>
2026-04-12 11:18:48 +0200

    Setup React + Vite + Tailwind en client

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-yellow-400 text-5xl font-bold">POWER PLAY</h1>
+    </div>
+  )
+}
